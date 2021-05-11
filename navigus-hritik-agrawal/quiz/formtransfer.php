44cb1b400e90a712d06469c640cb7d9b8c023376
Hritik Agrawal <62841487+hritik-agrawal@users.noreply.github.com>
2021-05-11 11:05:13 +0530

    Add files via upload

---
+++ navigus-hritik-agrawal/quiz/formtransfer.php
@@ -0,0 +1,47 @@
+<?php
+       
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                   
+    function get_data() {
+        $name = $_POST['name'];
+        $file_name='StudentsData'. '.json';
+   
+        if(file_exists("$file_name")) { 
+            $current_data=file_get_contents("$file_name");
+            $array_data=json_decode($current_data, true);
+                               
+            $extra=array(
+                'Question' => $_POST['question'],
+                'choice1' => $_POST['choice1'],
+                'choice2' => $_POST['choice2'],
+                'choice3' => $_POST['choice3'],
+                'choice4' => $_POST['choice4'],
+                'answer' => $_POST['answer'],
+            );
+            $array_data[]=$extra;
+            echo "file exist<br/>";
+            return json_encode($array_data);
+        }
+        else {
+            $datae=array();
+            $datae[]=array(
+                'Name' => $_POST['name'],
+                'Branch' => $_POST['branch'],
+                'Year' => $_POST['year'],
+            );
+            echo "file not exist<br/>";
+            return json_encode($datae);   
+        }
+    }
+  
+    $file_name='questions'. '.json';
+      
+    if(file_put_contents("$file_name", get_data())) {
+        echo 'success';
+    }                
+    else {
+        echo 'There is some error';                
+    }
+}
+       
+?>
